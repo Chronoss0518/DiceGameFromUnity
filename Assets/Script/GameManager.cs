305d5bf3e34f3f01b1c3df7c3d89b3e0d19ed78a
Chronoss0518 <sinyusuke5000@gmail.com>
2026-05-18 14:43:58 +0900

    TurnTypeのenumを作成

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
 
-
-
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +14,79 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        turnList[(int)nowTurnType].Update();
+
+        if (!changeTurnFlg) return;
+        nowTurnType = nextTurn;
+        nextTurn = (TurnType)((int)(nextTurn) + 1 % (int)(TurnType.TurnEnd + 1));
+        changeTurnFlg = false;
     }
 
+    public enum TurnType
+    {
+        TurnStart,
+        FutureAttackCheck,
+        IceCheck,
+        StanCheck,
+        GuardEndCheck,
+        DoubleEndCheck,
+        PandoraDiceCheck,
+        SelectAction,//Playerのアクション//
+        DiceRollAction,//Playerのアクション//
+        DiceEffect,
+        PandoraDiceStartCheck,
+        PandoraDiceCharaSelectAction,//Playerのアクション//
+        PandoraDiceRollAction,//Playerのアクション//
+        TurnEnd,
+    }
 
+    public void SetSelectCharacter(int _no)
+    {
+        if (_no < 0) return;
+        if (characterList.Count <= _no) return ;
+        selectPlayer = _no;
+    }
 
+    public Character GetNowCharacter()
+    {
+        if (characterList.Count <= nowPlayer) return null;
+        return characterList[nowPlayer];
+    }
 
+    public Character GetSelectCharacter()
+    {
+        if (characterList.Count <= selectPlayer) return null;
+        return characterList[selectPlayer];
+    }
+
+    abstract public class TurnBase
+    {
+        public void SetGameManager(GameManager _manager) { gameManager = _manager; }
+
+        public TurnType GetNowTurnType() { return gameManager.nowTurnType; }
+
+        public void SetNowTurnType(TurnType _type) { gameManager.nowTurnType = _type; }
+
+        public Character GetNowCharacter() { return gameManager.GetNowCharacter(); }
+
+        public Character GetSelectCharacter() { return gameManager.GetSelectCharacter(); }
+
+        public bool IsPandoraDice() { return gameManager.pandoraDiceFlg; }
+
+        protected void ChangeTurn() { gameManager.changeTurnFlg = true; }
+
+        abstract public void Update();
+
+        GameManager gameManager = null;
+    };
+
+    TurnBase[] turnList = new TurnBase[(int)(TurnType.TurnEnd) + 1];
+    TurnType nowTurnType = TurnType.TurnStart;
+    TurnType nextTurn = TurnType.FutureAttackCheck;
+    bool changeTurnFlg = false;
+
+    int nowPlayer = 0;
+    int selectPlayer = 0;
+    bool pandoraDiceFlg = false;
+    List<Character>characterList = new List<Character>();
 }
