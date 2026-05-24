8e4a054c83ac75275952e6b5ce656c33f438c650
Chronoss0518 <sinyusuke5000@gmail.com>
2026-05-24 21:11:26 +0900

    Animationを扱うPrefabで利用するScriptを用意

---
+++ Assets/Animation/AnimationPrefabBase.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class AnimationPrefabBase : MonoBehaviour
+{
+    public bool isAnimationEnd { get; private set; } = false;
+
+}
